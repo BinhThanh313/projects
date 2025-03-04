19b881b62afe88e96a5803607398f45c52cc8754
BinhThanh313 <binhdt1303@gmail.com>
2025-03-04 23:00:07 +0700

    responsive 4 thang 3

--- sachtruyen/resources/views/admincp/chapter/index.blade.php
+++ sachtruyen/resources/views/admincp/chapter/index.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
 @include('layouts.nav')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8" style="    width: 100%;">
+    <div class="row justify-content-center" >
+        <div class="col-12 col-md-8" style="    width: 100%;"> <!-- Thay col-md-8 thành col-12 col-md-8 -->
             <div class="card">
                 <div class="card-header">Liệt kê chapter</div>
 
@@ -16,48 +15,70 @@
                         </div>
                     @endif
 
-                    <table class="table table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Tên chapter</th>
-                        <th scope="col">Slug chapter</th>
-                        <th scope="col">Tóm tắt</th>
-                        <th scope="col">Thuộc truyện</th>
-                        <th scope="col">Kích hoạt</th>
-                        <th scope="col">Quản lý</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($chapter as $key => $chap)
-                        <tr>
-                            <th scope="row">{{$key}}</th>
-                            <td>{{$chap->tieude}}</td>
-                            <td>{{$chap->slug_chapter}}</td>
-                            <td>{{$chap->tomtat}}</td>
-                            <td>{{$chap->truyen->tentruyen}}</td>
-                            <td>
-                                @if ($chap->kichhoat == 0)
-                                    <span class="text text-success">Kích hoạt</span>
-                                @else
-                                    <span class="text text-danger">Không kích hoạt</span>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="{{route('chapter.edit', [$chap->id])}}" class="btn btn-primary">Edit</a>
-                                <form action="{{route('chapter.destroy', [$chap->id])}}" method="post" style="display:inline-block;">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button onclick="return confirm('Bạn muốn xóa chapter này không ?')" class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                    </table>
+                    <!-- Thêm wrapper cho table để scroll ngang trên mobile -->
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Tên chapter</th>
+                                    <th scope="col" class="d-none d-lg-table-cell">Slug chapter</th> <!-- Ẩn trên mobile nhỏ -->
+                                    <th scope="col" class="d-none d-md-table-cell">Tóm tắt</th> <!-- Ẩn trên mobile -->
+                                    <th scope="col">Thuộc truyện</th>
+                                    <th scope="col">Kích hoạt</th>
+                                    <th scope="col">Quản lý</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($chapter as $key => $chap)
+                                    <tr>
+                                        <th scope="row">{{ $key }}</th>
+                                        <td>{{ $chap->tieude }}</td>
+                                        <td class="d-none d-lg-table-cell">{{ $chap->slug_chapter }}</td>
+                                        <td class="d-none d-md-table-cell">{{ $chap->tomtat }}</td>
+                                        <td>{{ $chap->truyen->tentruyen }}</td>
+                                        <td>
+                                            @if ($chap->kichhoat == 0)
+                                                <span class="text text-success">Kích hoạt</span>
+                                            @else
+                                                <span class="text text-danger">Không kích hoạt</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <div class="d-flex flex-column flex-md-row gap-2">
+                                                <a href="{{route('chapter.edit', [$chap->id])}}" 
+                                                   class="btn btn-primary btn-sm">Edit</a>
+                                                <form action="{{route('chapter.destroy', [$chap->id])}}" 
+                                                      method="post" 
+                                                      class="d-inline">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button onclick="return confirm('Bạn muốn xóa chapter này không ?')" 
+                                                            class="btn btn-danger btn-sm">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    /* Thêm CSS tùy chỉnh nếu cần */
+    @media (max-width: 768px) {
+        .card-header {
+            font-size: 1.1rem;
+        }
+        
+        .btn-sm {
+            width: 100%;
+        }
+    }
+</style>
 @endsection
--- sachtruyen/resources/views/admincp/truyen/index.blade.php
+++ sachtruyen/resources/views/admincp/truyen/index.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-
 @include('layouts.nav')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8" style="    width: 100%;">
+        <div class="col-12 col-md-8" style="    width: 100%;"> <!-- Thay col-md-8 thành col-12 col-md-8 -->
             <div class="card">
-                <div class="card-header">Liệt kê  truyện</div>
+                <div class="card-header">Liệt kê truyện</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,51 +15,77 @@
                         </div>
                     @endif
 
+                    <!-- Thêm wrapper cho table để scroll ngang trên mobile -->
+                    <div class="table-responsive">
                         <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Tên truyện</th>                
-                    <th scope="col">Hình ảnh</th>
-                    <th scope="col">Slug truyện</th>
-                    <th scope="col">Tóm tắt</th>
-                    <th scope="col">Danh mục</th>
-                    <th scope="col">Kích hoạt</th>
-                    <th scope="col">Quản lý</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($list_truyen as $key => $truyen)
-                    <tr>
-                        <th scope="row">{{ $key }}</th>
-                        <td>{{ $truyen->tentruyen }}</td>
-                        <td><img src="{{ asset('public/uploads/truyen/'.$truyen->hinhanh) }}" height="200" width="150"></td>
-                        <td>{{ $truyen->slug_truyen }}</td>
-                        <td>{{ $truyen->tomtat }}</td>
-                        <td>{{ $truyen->danhmuctruyen->tendanhmuc }}</td>
-                        <td>
-                            @if ($truyen->kichhoat == 0)
-                                <span class="text text-success">Kích hoạt</span>
-                            @else
-                                <span class="text text-danger">Không kích hoạt</span>
-                            @endif
-                            </td>
-                            <td>
-                                <a href="{{route('truyen.edit', [$truyen -> id])}}" class="btn btn-primary">Edit</a>
-                                <form action="{{route('truyen.destroy', [$truyen -> id])}}" method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button onclick="return confirm('Bạn muốn xóa truyện này không ?')" class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    
-
-                    
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Tên truyện</th>
+                                    <th scope="col" class="d-none d-md-table-cell">Hình ảnh</th> <!-- Ẩn trên mobile -->
+                                    <th scope="col" class="d-none d-lg-table-cell">Slug truyện</th> <!-- Ẩn trên mobile nhỏ -->
+                                    <th scope="col" class="d-none d-md-table-cell">Tóm tắt</th> <!-- Ẩn trên mobile -->
+                                    <th scope="col">Danh mục</th>
+                                    <th scope="col">Kích hoạt</th>
+                                    <th scope="col">Quản lý</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($list_truyen as $key => $truyen)
+                                    <tr>
+                                        <th scope="row">{{ $key }}</th>
+                                        <td>{{ $truyen->tentruyen }}</td>
+                                        <td class="d-none d-md-table-cell">
+                                            <img src="{{ asset('public/uploads/truyen/'.$truyen->hinhanh) }}" 
+                                                 class="img-fluid" 
+                                                 style="max-height: 200px; max-width: 150px;" 
+                                                 alt="{{ $truyen->tentruyen }}">
+                                        </td>
+                                        <td class="d-none d-lg-table-cell">{{ $truyen->slug_truyen }}</td>
+                                        <td class="d-none d-md-table-cell">{{ $truyen->tomtat }}</td>
+                                        <td>{{ $truyen->danhmuctruyen->tendanhmuc }}</td>
+                                        <td>
+                                            @if ($truyen->kichhoat == 0)
+                                                <span class="text text-success">Kích hoạt</span>
+                                            @else
+                                                <span class="text text-danger">Không kích hoạt</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <div class="d-flex flex-column flex-md-row gap-2">
+                                                <a href="{{route('truyen.edit', [$truyen->id])}}" 
+                                                   class="btn btn-primary btn-sm">Edit</a>
+                                                <form action="{{route('truyen.destroy', [$truyen->id])}}" 
+                                                      method="post" 
+                                                      class="d-inline">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button onclick="return confirm('Bạn muốn xóa truyện này không ?')" 
+                                                            class="btn btn-danger btn-sm">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+    /* Thêm CSS tùy chỉnh nếu cần */
+    @media (max-width: 768px) {
+        .card-header {
+            font-size: 1.1rem;
+        }
+        
+        .btn-sm {
+            width: 100%;
+        }
+    }
+</style>
 @endsection
